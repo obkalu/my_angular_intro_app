aa355dcd448a037114aaa4d72d8cd509c51c3bdf
Obinna A. Kalu <obkalu@gmail.com>
2017-02-19 18:22:07 -0600

    Initial commit of finished code

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: `
+              <ul>
+                <li><a routerLink="/">Home</a></li>
+                <li><a routerLink="/about">About</a></li>
+              </ul>
+              <router-outlet></router-outlet>
+            `
+})
+export class AppComponent  { }
---
+++ app/components/user.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { PostsService } from '../services/posts.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [PostsService]
+})
+export class UserComponent  { 
+  name : string; 
+  email : string;
+  address : address;
+  hobbies : string[];
+  showHobbbies : boolean;
+  posts : Post[];
+
+  constructor(private postsService : PostsService) {
+      this.name = 'Obinna A. Kalu'; 
+      this.email = 'obkalu@gmail.com';
+      this.address = {
+                    street: '8501 West Chester Avenue',
+                    city: 'Phoenix',
+                    state: 'AZ',
+                    zipcode: '85020'
+      }
+      this.hobbies = ['Music', 'Movies', 'Football'];
+      this.showHobbbies = false;
+
+      this.postsService.getPosts().subscribe(posts => {
+        this.posts = posts;
+      });
+  }
+
+  toggleHobbies() {
+    if(this.showHobbbies == true) {
+      this.showHobbbies = false;
+    } else {
+      this.showHobbbies = true;
+    }
+  }
+
+  addHobby(hobby:string) {
+    this.hobbies.push(hobby);
+    hobby = ''; 
+  }
+
+  deleteHobby(i:number) {
+    this.hobbies.splice(i, 1);
+  }
+
+
+} // End of UserComponent class definition
+
+interface address {
+    street : string;
+    city : string;
+    state : string;
+    zipcode : string;
+} // End of address interface definition
+
+interface Post {
+  id : number;
+  title : string;
+  body : string;
+}
